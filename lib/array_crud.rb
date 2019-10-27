21adb45d8b43d22ba7f6d32331e010344ff8cc9f
Adrian Mohabir <mohabiradrian@gmail.com>
2019-10-27 23:10:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
-  
+ cake = ["double fudge", "eminem", "black forest", "seven layer"]
+ 
 end
 
 def retrieve_element_from_index(array, index_number)
