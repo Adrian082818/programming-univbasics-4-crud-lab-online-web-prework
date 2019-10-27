48ff3c4e002833059c81089b0d8883a8f982882a
Adrian Mohabir <mohabiradrian@gmail.com>
2019-10-27 23:48:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
- cake = ["double fudge", "eminem", "black forest", "seven layer"]
+ cake = ["I", "am", "really", "learning"]
   array.unshift("wow")
 end
 
