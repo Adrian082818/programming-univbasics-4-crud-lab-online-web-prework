fa193cbafbe25832613014e7517a92bfb2b426d0
Adrian Mohabir <mohabiradrian@gmail.com>
2019-10-27 23:50:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
  cake = ["wow", "I", "am", "really", "learning"]
- wow = cake.shift
+ "wow" = cake.shift
 end
 
 def retrieve_element_from_index(array, index_number)
